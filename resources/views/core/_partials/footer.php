03a4be8b05bdd482958a2ed26a44f5325baccaac
MartMbithi <martdevelopers254@gmail.com>
2020-09-23 12:03:50 +0300

    Partialized Partials

---
+++ resources/views/core/_partials/footer.php
@@ -0,0 +1,14 @@
+<footer class="footer">
+    <div class="footer__left">
+        <ul class="footer__info">
+            <li>© 2020 - <?php echo date('Y');?> KEA-Hotel-ERP. Proudly Powered By <a href="https://martdev.info">MartDevelopers Inc</a></li>
+            <li><a href="text-page.html">Terms & Conditions</a></li>
+            <li><a href="text-page.html">Privacy Policy</a></li>
+        </ul>
+    </div>
+    <ul class="footer__social">
+        <li><a href="#" class="social-link"><i class="icon-facebook-alt"></i></a></li>
+        <li><a href="#" class="social-link"><i class="icon-twitter-alt"></i></a></li>
+        <li><a href="#" class="social-link"><i class="icon-instagram"></i></a></li>
+    </ul>
+</footer>
---
+++ resources/views/core/_partials/header.php
@@ -0,0 +1,24 @@
+<header class="header">
+    <nav class="navbar navbar-white navbar-overlay">
+        <a class="logo-link" href="home.html">
+            <img class="logotype" src="assets/images/ui/logo-white.svg" alt="Sochi">
+        </a>
+        <div class="navbar__menu">
+            <button class="hamburger" type="button">
+                <span></span>
+                <span></span>
+            </button>
+            <ul class="nav">
+                <li class="nav__item _is-current"><a class="nav__link" href="rooms.html"><span data-hover="Rooms">Rooms</span></a></li>
+                <li class="nav__item"><a class="nav__link" href="gallery.html"><span data-hover="Gallery">Gallery</span></a></li>
+                <li class="nav__item"><a class="nav__link" href="about.html"><span data-hover="About Us">About Us</span></a></li>
+                <li class="nav__item"><a class="nav__link" href="blog.html"><span data-hover="Blog">Blog</span></a></li>
+                <li class="nav__item"><a class="nav__link" href="contact.html"><span data-hover="Contact Us">Contact Us</span></a></li>
+                <li class="nav__item"><a class="btn btn__medium" href="#"><i class="btn-icon-left icon-bookmark"></i>Reservations</a></li>
+            </ul>
+        </div>
+        <div class="navbar__btn">
+            <a class="btn btn__medium" href="rooms.html"><i class="btn-icon-left icon-bookmark"></i>Reservations</a>
+        </div>
+    </nav>
+</header>
