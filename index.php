2cf222732dcdb5750b79d0a2adbcd2dd42ddf826
MartMbithi <martdevelopers254@gmail.com>
2020-09-23 11:52:29 +0300

    Indexed Index File

---
+++ index.php
@@ -0,0 +1,11 @@
+<?php
+	if (!empty($_SERVER['HTTPS']) && ('on' == $_SERVER['HTTPS'])) {
+		$uri = 'https://';
+	} else {
+		$uri = 'http://';
+	}
+	$uri .= $_SERVER['HTTP_HOST'];
+	header('Location: '.$uri.'/KEA-HOTEL-ERP/resources/views/core/');
+	exit;
+?>
+Something is wrong here and i can feel it
