d28c1ae2043ca2d16085e48717b8521b117f2845
MartMbithi <martdevelopers254@gmail.com>
2020-09-24 11:26:04 +0300

    Full CRUD on HRM

--- resources/views/core/staff/admin/view_staff.php
+++ resources/views/core/staff/admin/view_staff.php
@@ -45,7 +45,7 @@ require_once('partials/_head.php');
                                     <li class="breadcrumb-item"><a href="dashboard.php">Dashboard</a></li>
                                     <li class="breadcrumb-item"><a href="dashboard.php">Dashboard</a></li>
                                     <li class="breadcrumb-item"><a href="manage_staffs.php">HRM</a></li>
-                                    <li class="breadcrumb-item active" aria-current="page"><span><?php echo $row->name;?> Profile</span></li>
+                                    <li class="breadcrumb-item active" aria-current="page"><span><?php echo $row->name; ?> Profile</span></li>
                                 </ol>
                             </nav>
 
@@ -78,7 +78,7 @@ require_once('partials/_head.php');
                             <div class="user-profile layout-spacing">
                                 <div class="widget-content widget-content-area">
                                     <div class="d-flex justify-content-between">
-                                        <h3 class="">Profile</h3>
+                                        <h3 class="text-warning">Profile</h3>
                                         <a href="update_staff.php?update=<?php echo $row->id; ?>" class="mt-2 edit-profile"> <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-edit-3">
                                                 <path d="M12 20h9"></path>
                                                 <path d="M16.5 3.5a2.121 2.121 0 0 1 3 3L7 19l-4 1 1-4L16.5 3.5z"></path>
@@ -86,27 +86,28 @@ require_once('partials/_head.php');
                                     </div>
                                     <div class="text-center user-info">
                                         <?php echo $profile; ?>
-                                        <p class=""><?php echo $row->name; ?></p>
+                                        <p class="text-warning"><?php echo $row->name; ?></p>
                                     </div>
                                     <div class="user-info-list">
 
                                         <div class="">
                                             <ul class="contacts-block list-unstyled">
                                                 <li class="contacts-block__item">
-                                                    <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-coffee">
-                                                        <path d="M18 8h1a4 4 0 0 1 0 8h-1"></path>
-                                                        <path d="M2 8h16v9a4 4 0 0 1-4 4H6a4 4 0 0 1-4-4V8z"></path>
-                                                        <line x1="6" y1="1" x2="6" y2="4"></line>
-                                                        <line x1="10" y1="1" x2="10" y2="4"></line>
-                                                        <line x1="14" y1="1" x2="14" y2="4"></line>
-                                                    </svg> <?php echo $row->number; ?>
+                                                    <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-activity">
+                                                        <path d="M16 21v-2a4 4 0 0 0-4-4H5a4 4 0 0 0-4 4v2"></path>
+                                                        <circle cx="8.5" cy="7" r="4"></circle>
+                                                        <polyline points="17 11 19 13 23 9"></polyline>
+                                                    </svg>
+                                                    <?php echo $row->number; ?>
                                                 </li>
-                                                
+
                                                 <li class="contacts-block__item">
-                                                    <a href="mailto:<?php echo $row->email; ?>"><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-mail">
+                                                    <a href="mailto:<?php echo $row->email; ?>">
+                                                        <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-mail">
                                                             <path d="M4 4h16c1.1 0 2 .9 2 2v12c0 1.1-.9 2-2 2H4c-1.1 0-2-.9-2-2V6c0-1.1.9-2 2-2z"></path>
                                                             <polyline points="22,6 12,13 2,6"></polyline>
-                                                        </svg><?php echo $row->email; ?></a>
+                                                        </svg><?php echo $row->email; ?>
+                                                    </a>
                                                 </li>
                                                 <li class="contacts-block__item">
                                                     <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-phone">
@@ -115,9 +116,11 @@ require_once('partials/_head.php');
                                                 </li>
 
                                                 <li class="contacts-block__item">
-                                                    <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-phone">
-                                                        <path d="M22 16.92v3a2 2 0 0 1-2.18 2 19.79 19.79 0 0 1-8.63-3.07 19.5 19.5 0 0 1-6-6 19.79 19.79 0 0 1-3.07-8.67A2 2 0 0 1 4.11 2h3a2 2 0 0 1 2 1.72 12.84 12.84 0 0 0 .7 2.81 2 2 0 0 1-.45 2.11L8.09 9.91a16 16 0 0 0 6 6l1.27-1.27a2 2 0 0 1 2.11-.45 12.84 12.84 0 0 0 2.81.7A2 2 0 0 1 22 16.92z"></path>
-                                                    </svg> <?php echo $row->adr; ?>
+                                                    <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-activity">
+                                                        <path d="M21 10c0 7-9 13-9 13s-9-6-9-13a9 9 0 0 1 18 0z"></path>
+                                                        <circle cx="12" cy="10" r="3"></circle>
+                                                    </svg>
+                                                    <?php echo $row->adr; ?>
                                                 </li>
 
                                             </ul>
@@ -249,8 +252,8 @@ require_once('partials/_head.php');
 
                             <div class="bio layout-spacing ">
                                 <div class="widget-content widget-content-area">
-                                    <h3 class="">Bio </h3>
-                                    <?php echo $row->doc_bio; ?>
+                                    <h3 class="text-warning">Bio </h3>
+                                    <?php echo $row->bio; ?>
                                     <br>
                                     <!-- <div class="bio-skill-box">
 
