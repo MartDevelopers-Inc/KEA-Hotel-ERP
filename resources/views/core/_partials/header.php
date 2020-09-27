497d96d9a27fb49dac4a6a67e39417faab96a3ac
MartMbithi <martdevelopers254@gmail.com>
2020-09-27 20:11:55 +0300

    Landing Pages - Rooms

--- resources/views/core/_partials/header.php
+++ resources/views/core/_partials/header.php
@@ -9,11 +9,11 @@
                 <span></span>
             </button>
             <ul class="nav">
-                <li class="nav__item _is-current"><a class="nav__link" href="rooms.html"><span data-hover="Rooms">Rooms</span></a></li>
-                <li class="nav__item"><a class="nav__link" href="gallery.html"><span data-hover="Gallery">Gallery</span></a></li>
-                <li class="nav__item"><a class="nav__link" href="about.html"><span data-hover="About Us">About Us</span></a></li>
-                <li class="nav__item"><a class="nav__link" href="blog.html"><span data-hover="Blog">Blog</span></a></li>
-                <li class="nav__item"><a class="nav__link" href="contact.html"><span data-hover="Contact Us">Contact Us</span></a></li>
+                <li class="nav__item _is-current"><a class="nav__link" href="rooms.php"><span data-hover="Rooms">Rooms</span></a></li>
+                <li class="nav__item"><a class="nav__link" href="gallery.php"><span data-hover="Gallery">Gallery</span></a></li>
+                <li class="nav__item"><a class="nav__link" href="about.php"><span data-hover="About Us">About Us</span></a></li>
+                <li class="nav__item"><a class="nav__link" href="blog.php"><span data-hover="Blog">Blog</span></a></li>
+                <li class="nav__item"><a class="nav__link" href="contact.php"><span data-hover="Contact Us">Contact Us</span></a></li>
                 <li class="nav__item"><a class="btn btn__medium" href="#"><i class="btn-icon-left icon-bookmark"></i>Reservations</a></li>
             </ul>
         </div>
