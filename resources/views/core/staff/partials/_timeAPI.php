7a4dc01f7c3c46f54b9212717975c6a42a3911f5
MartMbithi <martdevelopers254@gmail.com>
2020-09-28 12:48:16 +0300

    Minor Update On Time API

--- resources/views/core/staff/partials/_timeAPI.php
+++ resources/views/core/staff/partials/_timeAPI.php
@@ -1,8 +1,7 @@
 <?php
 $login_id = $_SESSION['id'];
-$ret = "SELECT  * FROM  staffs  WHERE id=?";
+$ret = "SELECT  * FROM  staffs  WHERE id= '$login_id'";
 $stmt = $mysqli->prepare($ret);
-$stmt->bind_param('i', $login_id);
 $stmt->execute(); //ok
 $res = $stmt->get_result();
 //$cnt=1;
