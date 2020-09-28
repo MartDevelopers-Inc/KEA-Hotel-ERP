7aa253d69afe0f68880e74a357bdd45cbaed5fad
MartMbithi <martdevelopers254@gmail.com>
2020-09-28 12:21:37 +0300

    Module Compatibility Fixes

--- resources/views/core/staff/partials/_timeAPI.php
+++ resources/views/core/staff/partials/_timeAPI.php
@@ -1,6 +1,6 @@
 <?php
 $login_id = $_SESSION['id'];
-$ret="SELECT  * FROM  admin  WHERE id=?";
+$ret="SELECT  * FROM  staffs  WHERE id=?";
 $stmt= $mysqli->prepare($ret) ;
 $stmt->bind_param('i',$login_id);
 $stmt->execute() ;//ok
@@ -35,5 +35,5 @@ while($row=$res->fetch_object())
     $d_time = "Good Night";
     }
 ?>
-<li><a href="javascript:void(0);"  class="tabmenu"><?php echo $d_time;?> <?php echo $row->username;?></a></li></h3>
+<li><a href="javascript:void(0);"  class="tabmenu"><?php echo $d_time;?> <?php echo $row->name;?></a></li></h3>
 <?php }?>
