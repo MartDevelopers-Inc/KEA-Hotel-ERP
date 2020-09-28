c719758d0eb0f5f15c99420d9536088cc372f378
MartMbithi <martdevelopers254@gmail.com>
2020-09-28 12:25:31 +0300

    Full Auth On Staffs

---
+++ resources/views/core/staff/index.php
@@ -0,0 +1,94 @@
+<?php
+session_start();
+include('configs/config.php');
+//handle login
+if (isset($_POST['login'])) {
+    $email = $_POST['email'];
+    $password = sha1(md5($_POST['password'])); //double encrypt to increase security
+    $stmt = $mysqli->prepare("SELECT email, password, id  FROM staffs  WHERE (email =? AND password =?)");
+    $stmt->bind_param('ss', $email, $password); //bind fetched parameters
+    $stmt->execute(); //execute bind 
+    $stmt->bind_result($email, $password, $id); //bind result
+    $rs = $stmt->fetch();
+    $_SESSION['id'] = $id;
+    if ($rs) {
+        //if its sucessfull
+        header("location:dashboard.php");
+    } else {
+        $err = "Access Denied Please Check Your Credentials";
+    }
+}
+require_once('partials/_head.php');
+?>
+
+<body class="form">
+
+
+    <div class="form-container outer">
+        <div class="form-form">
+            <div class="form-form-wrap">
+                <div class="form-container">
+                    <div class="form-content">
+                        <div>
+                            <img src="assets/img/logo.png" alt="logo" class="img-fluid">
+                        </div>
+                        <form method="POST" class="text-left">
+                            <div class="form">
+
+                                <div id="username-field" class="field-wrapper input">
+                                    <label for="username">EMAIL</label>
+                                    <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-user">
+                                        <path d="M20 21v-2a4 4 0 0 0-4-4H8a4 4 0 0 0-4 4v2"></path>
+                                        <circle cx="12" cy="7" r="4"></circle>
+                                    </svg>
+                                    <input id="username" required name="email" type="email" class="form-control">
+                                </div>
+
+                                <div id="password-field" class="field-wrapper input mb-2">
+                                    <div class="d-flex justify-content-between">
+                                        <label for="password">PASSWORD</label>
+                                        <a href="reset_password.php" class="forgot-pass-link">Forgot Password?</a>
+                                    </div>
+                                    <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-lock">
+                                        <rect x="3" y="11" width="18" height="11" rx="2" ry="2"></rect>
+                                        <path d="M7 11V7a5 5 0 0 1 10 0v4"></path>
+                                    </svg>
+                                    <input id="password" required name="password" type="password" class="form-control">
+                                    <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" id="toggle-password" class="feather feather-eye">
+                                        <path d="M1 12s4-8 11-8 11 8 11 8-4 8-11 8-11-8-11-8z"></path>
+                                        <circle cx="12" cy="12" r="3"></circle>
+                                    </svg>
+                                </div>
+                                <div class="d-sm-flex justify-content-between">
+                                    <div class="field-wrapper">
+                                        <button type="submit" name="login" class="btn btn-primary mixin" value="">Log In</button>
+                                    </div>
+                                </div>
+
+                                <!-- <div class="division">
+                                    <span>OR</span>
+                                </div>
+
+                                <div class="social">
+                                    <a href="doc/" class="btn social-fb">
+                                        <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-facebook">
+                                            <path d="M18 2h-3a5 5 0 0 0-5 5v3H7v4h3v8h4v-8h3l1-4h-4V7a1 1 0 0 1 1-1h3z"></path>
+                                        </svg>
+                                        <span class="brand-name">Login In As Doctor</span>
+                                    </a>
+                                </div>-->
+
+                                <p class="signup-link"><a href="../">Home</a></p>
+                            </div>
+                        </form>
+
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <?php require_once('partials/_scripts.php'); ?>
+</body>
+
+</html>
--- resources/views/core/staff/partials/_timeAPI.php
+++ resources/views/core/staff/partials/_timeAPI.php
@@ -1,39 +1,28 @@
 <?php
 $login_id = $_SESSION['id'];
-$ret="SELECT  * FROM  staffs  WHERE id=?";
-$stmt= $mysqli->prepare($ret) ;
-$stmt->bind_param('i',$login_id);
-$stmt->execute() ;//ok
-$res=$stmt->get_result();
+$ret = "SELECT  * FROM  staffs  WHERE id=?";
+$stmt = $mysqli->prepare($ret);
+$stmt->bind_param('i', $login_id);
+$stmt->execute(); //ok
+$res = $stmt->get_result();
 //$cnt=1;
-while($row=$res->fetch_object())
-{
+while ($row = $res->fetch_object()) {
     // time function to get day zones ie morning, noon, and night.
     $t = date("H");
 
-    if ($t < "10")
-    {
-    $d_time = "Good Morning";
+    if ($t < "10") {
+        $d_time = "Good Morning";
+    } elseif ($t < "15") {
 
-    }
+        $d_time =  "Good Afternoon";
+    } elseif ($t < "20") {
 
-    elseif ($t < "15")
-    {
+        $d_time =  "Good Evening";
+    } else {
 
-    $d_time =  "Good Afternoon";
-
-    } 
-
-    elseif ($t < "20")
-    {
-
-    $d_time =  "Good Evening";
-
-    } 
-    else {
-
-    $d_time = "Good Night";
+        $d_time = "Good Night";
     }
 ?>
-<li><a href="javascript:void(0);"  class="tabmenu"><?php echo $d_time;?> <?php echo $row->name;?></a></li></h3>
-<?php }?>
+    <li><a href="javascript:void(0);" class="tabmenu"><?php echo $d_time; ?> <?php echo $row->name; ?></a></li>
+    </h3>
+<?php } ?>
