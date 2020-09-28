f5bf62f977e66044e91983357599d186e9ad9f24
MartMbithi <martdevelopers254@gmail.com>
2020-09-28 14:04:33 +0300

    Room Reports

--- resources/views/core/staff/partials/_sidebar.php
+++ resources/views/core/staff/partials/_sidebar.php
@@ -154,32 +154,7 @@
                 </ul>
             </li>
 
-            <li class="menu">
-                <a href="#inventory" data-toggle="collapse" aria-expanded="false" class="dropdown-toggle">
-                    <div class="">
-                        <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-clipboard">
-                            <path d="M16 4h2a2 2 0 0 1 2 2v14a2 2 0 0 1-2 2H6a2 2 0 0 1-2-2V6a2 2 0 0 1 2-2h2"></path>
-                            <rect x="8" y="2" width="8" height="4" rx="1" ry="1"></rect>
-                        </svg>
-                        <span>Inventory</span>
-                    </div>
-                    <div>
-                        <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-chevron-right">
-                            <polyline points="9 18 15 12 9 6"></polyline>
-                        </svg>
-                    </div>
-                </a>
-                <ul class="collapse submenu list-unstyled" id="inventory" data-parent="#accordionExample">
-                    <li>
-                        <a href="manage_inventory.php"> Manage Assets </a>
-                    </li>
-                    <li>
-                        <a href="rooms_inventory.php"> Rooms </a>
-                    </li>
-                </ul>
-            </li>
-
-            
+                        
             <li class="menu">
                 <a href="#Reporting" data-toggle="collapse" aria-expanded="false" class="dropdown-toggle">
                     <div class="">
