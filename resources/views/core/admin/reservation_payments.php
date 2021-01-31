d60217a641dc90c72749ffe8022a3aafe011fb00
MartMbithi <martdevelopers254@gmail.com>
2021-01-31 15:12:58 +0300

    Reservations Payments Scaffolds

---
+++ resources/views/core/admin/reservation_payments.php
@@ -0,0 +1,85 @@
+<?php
+session_start();
+require_once('../config/config.php');
+require_once('../config/codeGen.php');
+require_once('../config/checklogin.php');
+sudo(); /* Invoke Admin Check Login */
+
+
+require_once("../partials/head.php");
+?>
+
+<body class="hold-transition sidebar-mini layout-fixed layout-navbar-fixed layout-footer-fixed">
+    <div class="wrapper">
+        <!-- Navbar -->
+        <?php require_once("../partials/admin_nav.php"); ?>
+        <!-- /.navbar -->
+
+        <!-- Main Sidebar Container -->
+        <?php require_once("../partials/admin_sidebar.php"); ?>
+
+        <!-- Content Wrapper. Contains page content -->
+        <div class="content-wrapper">
+            <!-- Content Header (Page header) -->
+            <section class="content-header">
+                <div class="container-fluid">
+                    <div class="row mb-2">
+                        <div class="col-sm-6">
+                            <h1>Reservations Paymets </h1>
+                        </div>
+                        <div class="col-sm-6">
+                            <ol class="breadcrumb float-sm-right">
+                                <li class="breadcrumb-item"><a href="#">Home</a></li>
+                                <li class="breadcrumb-item"><a href="dashboard.php">Dashboard</a></li>                                <li class="breadcrumb-item"><a href="dashboard.php">Dashboard</a></li>
+                                <li class="breadcrumb-item"><a href="reservations.php">Reservations</a></li>
+                                <li class="breadcrumb-item active">Payments</li>
+                            </ol>
+                        </div>
+                    </div>
+                </div>
+            </section>
+
+            <section class="content">
+                <div class="container-fluid">
+                    <form class="form-inline">
+                    </form>
+                    <div class="text-right">
+                        <button type="button" class="btn btn-primary" data-toggle="modal" data-target="#add_payment">Add Reservation Payment</button>
+                    </div>
+                    <!-- Add  Modal -->
+                    <div class="modal fade" id="add_payment">
+                        <div class="modal-dialog  modal-xl">
+                            <div class="modal-content">
+                                <div class="modal-header">
+                                    <h4 class="modal-title">Fill All Values </h4>
+                                    <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                                        <span aria-hidden="true">&times;</span>
+                                    </button>
+                                </div>
+                                <div class="modal-body">
+                                    
+                                </div>
+                                <div class="modal-footer justify-content-between">
+                                    <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <!-- End  Modal -->
+
+                    <hr>
+                    <div class="col-12">
+                       
+                    </div>
+                </div>
+            </section>
+        </div>
+        <?php require_once("../partials/footer.php"); ?>
+
+    </div>
+    <?php require_once("../partials/scripts.php"); ?>
+    <!-- Ajax Scripts -->
+   
+</body>
+
+</html>
